fa8daa123700909664177984cb6b6b13410a27b1
yunozino <rebbecaleticia4@gmail.com>
2023-10-18 16:54:43 -0300

    l

---
+++ main.py
@@ -0,0 +1,38 @@
+#programa que fala estação por numero do mês e dia
+
+print = ('qual a estação do ano?')
+dia = int (input('dia:\n'))
+mes = int (input('mes:\n'))
+
+def estacao (mes):
+    if mes in [1,2]:
+        return 'verao'
+    elif mes== 3:
+        if dia < 20:
+            return 'verao'
+        else:
+            return 'outono'
+    elif mes in [4,5]:
+        return 'outono'
+    elif mes == 6:
+        if dia < 21:
+            return 'outono'
+        else:
+            return 'inverno'
+    elif mes in [7,8]:
+        return 'inverno'
+    elif mes == 9:
+        if dia < 23:
+            return 'inverno'
+    else:
+        return 'primavera'
+    elif mes in (10,11):
+        return 'primavera'
+    elif mes 12:
+        if dia <  22:
+            return 'primavera'
+    else:
+        return 'verao'
+
+    resul: estacao (dia, mes)
+    print(resul)
